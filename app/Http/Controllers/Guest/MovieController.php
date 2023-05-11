40c2f4042b6285f10739ad794c5f758142415e98
DavideAnile <davide.anile16@gmail.com>
2023-05-11 14:54:16 +0200

    feat : end exercise

--- app/Http/Controllers/Guest/MovieController.php
+++ app/Http/Controllers/Guest/MovieController.php
@@ -3,9 +3,17 @@
 namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
+use App\Models\Movie;
 use Illuminate\Http\Request;
 
 class MovieController extends Controller
 {
-    //
+    public function index(){
+
+        $movies = Movie::all();
+
+       
+
+        return view('home', compact('movies'));
+    }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,24 @@
 @extends('layouts/main')
 
 @section('content')
-    <div class="container">
-        ciao
-    </div>
+<div class="row m-5">
+
+    
+        
+   @foreach ($movies as $singleMovie)
+       
+   <div class="card p-3 m-3" style="width: 18rem;">
+       <div class="card-body">
+         <h5 class="card-title">{{$singleMovie->title}}</h5>
+         <h6 class="card-subtitle mb-2 text-muted">{{$singleMovie->original_title}}</h6>
+         <ul class="list-group list-group-flush">
+            <li class="list-group-item">NATION : {{$singleMovie->nationality}}</li>
+            <li class="list-group-item">DATA : {{$singleMovie->date}}</li>
+            <li class="list-group-item">VOTE : {{$singleMovie->vote}}</li>
+          </ul>
+       </div>
+     </div>
+   @endforeach
+
+</div>
 @endsection
